4c42090e098319a2d14a31e4d6454af1bc45a458
Soren Larsen <iamsorenl@gmail.com>
2024-10-16 09:52:02 -0700

    one more step

--- Part_A/run.py
+++
@@ -1,1 +0,0 @@
-### Part A: Feature Engineering for Naive Bayes, SVM & Decision Tree
---
+++ part_a.py
@@ -0,0 +1,6 @@
+### Part A: Feature Engineering for Naive Bayes, SVM & Decision Tree
+import pandas as pd
+
+sbr = pd.read_csv('small_books_rating.csv')
+
+print(sbr.head())
